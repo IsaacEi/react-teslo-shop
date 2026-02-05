e044e9dde444418762c999b0b81c46d11bc42128
Isaac Cisneros <ing.cisnecar@gmail.com>
2026-02-04 21:23:00 -0600

    refactor: switch from createBrowserRouter to createHashRouter for routing

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { /* createBrowserRouter, */ createHashRouter } from "react-router";
 import { shopRouter } from "@/shop/router/shop.router";
 import { auntRouter } from "@/auth/router/auth.router";
 import { adminRouter } from "@/admin/router/admin.router";
 import { Navigate } from "react-router";
 
-export const appRouter = createBrowserRouter([
+/* export const appRouter = createBrowserRouter([ */
+export const appRouter = createHashRouter([
    shopRouter,
    auntRouter,
    adminRouter,
